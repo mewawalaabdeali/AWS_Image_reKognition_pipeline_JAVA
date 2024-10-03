c97b132036b752f8f7e9d733daf8da85c3b50461
ali <ali_abde37@outlook.com>
2024-10-03 13:52:00 -0400

    Committing Rekognition module

--- src/main/java/org/example/car_rekognition/Car_Rekognition.java
+++ src/main/java/org/example/car_rekognition/Car_Rekognition.java
@@ -1,13 +1,18 @@
 package org.example.car_rekognition;
 
-import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.rekognition.model.DetectLabelsRequest;
+import software.amazon.awssdk.services.rekognition.model.DetectLabelsResponse;
+import software.amazon.awssdk.services.rekognition.model.Label;
+
+import software.amazon.awssdk.services.rekognition.model.Image;
 import software.amazon.awssdk.services.s3.model.S3Object;
+import software.amazon.awssdk.services.rekognition.RekognitionClient;
+import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.ListObjectsV2Request;
 import software.amazon.awssdk.services.s3.model.ListObjectsV2Response;
 import software.amazon.awssdk.regions.Region;
-
 import software.amazon.awssdk.services.sqs.SqsClient;
-import software.amazon.awssdk.services.rekognition.RekognitionClient;
+
 
 
 import java.util.ArrayList;
@@ -23,6 +28,7 @@ public class Car_Rekognition {
         //Step2: We will read and list objects from s3
         ListObjectsV2Request listRequest = ListObjectsV2Request.builder().bucket(bucketName).build();
         ListObjectsV2Response response = s3.listObjectsV2(listRequest);
+
 
         //Step3: Checking image extensions
         List<String> imageFiles = new ArrayList<>();
@@ -44,6 +50,37 @@ public class Car_Rekognition {
         for (String imageFile : imageFiles){
             System.out.println(imageFile);
         }
+
+        //Now moving on to the part with Rekognition
+        //Step1 : initializing Rekognition Client
+        RekognitionClient rekognition = RekognitionClient.builder().region(Region.US_EAST_1).build();
+
+        //Now since we have processed the images with S3 above, we will ask rekognition to process only those images based on our filter
+        for (String imageFile:imageFiles){
+            DetectLabelsRequest detectRequest = DetectLabelsRequest.builder()
+                    .image(Image.builder().s3Object(software.amazon.awssdk.services.rekognition.model.S3Object.builder()
+                            .bucket(bucketName).name(imageFile).build()).build()).maxLabels(10).build();
+
+
+            //Calling rekognition to detect labels
+            DetectLabelsResponse detectResponse = rekognition.detectLabels(detectRequest);
+
+            //Checking for Car labels
+            boolean carDetected = false;
+            for (Label label: detectResponse.labels()){
+                if(label.name().equals("Car") && label.confidence()>=80.0f){
+                    System.out.println("Car detected in image : " +imageFile);
+                    carDetected = true;
+
+
+                }
+            }
+            if(!carDetected){
+                System.out.println("No car detected in image : " +imageFile);
+            }
+
+
+                    }
 
             }
         }
