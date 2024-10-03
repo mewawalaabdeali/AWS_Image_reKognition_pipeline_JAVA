5e663008d211d223b39af14514105d7609fefb52
ali <ali_abde37@outlook.com>
2024-10-03 11:24:30 -0400

    Committing S3 read and listing

--- src/main/java/org/example/car_rekognition/Car_Rekognition.java
+++ src/main/java/org/example/car_rekognition/Car_Rekognition.java
@@ -1,37 +1,52 @@
 package org.example.car_rekognition;
 
-
 import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.ListBucketsResponse;
-import software.amazon.awssdk.services.s3.model.Bucket;
+import software.amazon.awssdk.services.s3.model.S3Object;
+import software.amazon.awssdk.services.s3.model.ListObjectsV2Request;
+import software.amazon.awssdk.services.s3.model.ListObjectsV2Response;
+import software.amazon.awssdk.regions.Region;
+
 import software.amazon.awssdk.services.sqs.SqsClient;
 import software.amazon.awssdk.services.rekognition.RekognitionClient;
-import software.amazon.awssdk.regions.Region;
+
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Car_Rekognition {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
 
-        /*for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);*/
-
-
+        //Step1: Initializing the s3 client
         S3Client s3 = S3Client.builder().region(Region.US_EAST_1).build();
+        String bucketName = "my-image-recognition-demo-bucket";
 
-        ListBucketsResponse response = s3.listBuckets();
+        //Step2: We will read and list objects from s3
+        ListObjectsV2Request listRequest = ListObjectsV2Request.builder().bucket(bucketName).build();
+        ListObjectsV2Response response = s3.listObjectsV2(listRequest);
 
-        if (response.buckets().isEmpty()) {
-            System.out.println("No buckets found. ");
-        } else {
-            for (Bucket bucket : response.buckets()) {
-                System.out.println(" -- Bucket names: " + bucket.name());
+        //Step3: Checking image extensions
+        List<String> imageFiles = new ArrayList<>();
+
+        //Step4: Collecting image files from s3
+        for (S3Object object: response.contents()){
+            String key = object.key();
+
+            //filtering the files for extension
+            if(key.endsWith(".jpg") || key.endsWith("jpeg") || key.endsWith("png")){
+                System.out.println("Found Images : " + key);
+                imageFiles.add(key);
             }
         }
-    }
-}
+
+        //Checking the images found and printing them
+        System.out.println("Total Images found : " +imageFiles.size());
+        System.out.println("Images files found : ");
+        for (String imageFile : imageFiles){
+            System.out.println(imageFile);
+        }
+
+            }
+        }
+
 
 
