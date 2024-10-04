c072d0818fdae34355f13d55ded25d6abf98a3c4
ali <ali_abde37@outlook.com>
2024-10-04 13:25:27 -0400

    Reading SQS done as well

--- src/main/java/org/example/text_rekognition/Text_Rekognition.java
+++ src/main/java/org/example/text_rekognition/Text_Rekognition.java
@@ -3,12 +3,18 @@ package org.example.text_rekognition;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.rekognition.RekognitionClient;
 import software.amazon.awssdk.services.rekognition.model.DetectTextRequest;
+import software.amazon.awssdk.services.rekognition.model.DetectTextResponse;
 import software.amazon.awssdk.services.rekognition.model.Image;
+import software.amazon.awssdk.services.rekognition.model.TextDetection;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.S3Object;
 import software.amazon.awssdk.services.s3.model.ListObjectsV2Request;
 import software.amazon.awssdk.services.s3.model.ListObjectsV2Response;
 import software.amazon.awssdk.services.sqs.SqsClient;
+import software.amazon.awssdk.services.sqs.model.DeleteMessageRequest;
+import software.amazon.awssdk.services.sqs.model.ReceiveMessageRequest;
+import software.amazon.awssdk.services.sqs.model.Message;
+
 
 import java.util.ArrayList;
 import java.util.List;
@@ -53,8 +59,84 @@ public class Text_Rekognition {
                     .image(Image.builder().s3Object(software.amazon.awssdk.services.rekognition.model.S3Object.builder()
                             .bucket(bucketName).name(imageFile).build()).build()).build();
 
+            //Step4 : we will call rekognition to detect text
+            DetectTextResponse textResponse = rekognition.detectText(textRequest);
+
+            //Step5 : We will process the detected text
+            List<TextDetection> textDetections = textResponse.textDetections();
+            if(!textDetections.isEmpty()){
+                System.out.println("Text detected in images : " +imageFile);
+                for(TextDetection text: textDetections){
+                    if (text.confidence() >= 80.0f) {
+                        System.out.println("Detected Text : " +text.detectedText());
+                    }
+                }
+            }else {
+                System.out.println("No text detected in images : " + imageFile);
+            }
+
 
         }
+
+        //Parallely lets process SQS queue
+        //Initializing receive request method
+        ReceiveMessageRequest receiveRequest = ReceiveMessageRequest.builder().queueUrl(queueUrl)
+                .maxNumberOfMessages(10).waitTimeSeconds(20).build();
+
+        while(true){
+            List<Message> messages = sqs.receiveMessage(receiveRequest).messages();
+            for (Message message: messages){
+                String body = message.body();
+
+                //First we will check for termination signal(-1)
+                if (body.equals("-1")){
+                    System.out.println("End of Receiving messages");
+                    String messageReceiptHandle = message.receiptHandle();
+                    sqs.deleteMessage(DeleteMessageRequest.builder()
+                            .queueUrl(queueUrl).receiptHandle(messageReceiptHandle)
+                            .build());
+                    return;
+
+                }
+
+                //Here we check the message
+                for (String imageFile : imageFiles){
+                    if(imageFile.equals(body)){
+                        DetectTextRequest textRequest = DetectTextRequest.builder()
+                                .image(Image.builder()
+                                        .s3Object(software.amazon.awssdk.services.rekognition.model.S3Object.builder()
+                                                .bucket(bucketName).name(imageFile).build()).build()).build();
+                        //Call rekognition to detect text
+                        DetectTextResponse textResponse = rekognition.detectText(textRequest);
+
+                        //Now we will process the detected text
+                        List<TextDetection> textDetections = textResponse.textDetections();
+                        if(!textDetections.isEmpty()){
+                            System.out.println("Text Detected in image : " +imageFile);
+                            for (TextDetection text : textDetections){
+                                if(text.confidence()>=80.0f){
+                                    System.out.println("Detected Text " + text.detectedText());
+
+                                }
+                            }
+                        }else{
+                            System.out.println("No text detected in image : " + imageFile);
+                        }
+                        String messageReceiptHandle = message.receiptHandle();
+                        sqs.deleteMessage(DeleteMessageRequest.builder()
+                                .queueUrl(queueUrl).receiptHandle(messageReceiptHandle)
+                                .build());
+
+                    }
+                }
+
+            }
+
+
+        }
+
+
+
 
 
 
